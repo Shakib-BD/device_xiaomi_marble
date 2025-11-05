0d086b39431c0b8e50c4a03213643aca00e9fbe3
Shakib <dev@shakib.my.id>
2025-11-05 09:28:10 +0600

    marble: Bringup Halcyonify
    Signed-off-by: Shakib <dev@shakib.my.id>

--- halcyon_marble.mk
+++ halcyon_marble.mk
@@ -7,10 +7,10 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common Lineage configuration
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := halcyon_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -29,18 +29,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_DISABLE_EPPE := true
 TARGET_ENABLE_BLUR := true
 TARGET_INCLUDES_LOS_PREBUILTS := true
-
-# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
-AXION_MAINTAINER := Ashwani
-
-# Processor name (use "_" for spaces)
-AXION_PROCESSOR := Snapdragon®_7+_Gen_2_(SM7475)
-
-# Define rear camera specs (multiple sensors supported)
-AXION_CAMERA_REAR_INFO := 64,8,2  # Example: 50MP + 48MP
-
-# Define front camera specs
-AXION_CAMERA_FRONT_INFO := 16  # Example: 42MP
 
 # Flash Strength
 TORCH_STR_SUPPORTED := true
